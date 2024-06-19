78750aa7bcfddb8c49867df1a63ed408625af648
ali hasan <alihassanraja06@gmail.com>
2024-06-19 16:05:52 +0500

    card added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#form");
 const city = document.querySelector("#city");
-const card = document.querySelector("#card");
+const card = document.querySelector("#card1");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
